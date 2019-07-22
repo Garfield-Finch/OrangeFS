140641480357760ed21623cde20e2504c3f4b861
Garfield-Finch <tuzhitz@163.com>
2019-07-22 17:44:48 +0800

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 # set alias for this script
 alias empty_env='rm /mnt/test.* ~/test.*'
 # init results.txt file
-rm ~/results.txt
+# rm ~/results.txt
 cd ~
 # init test name
 tsnm="test"
@@ -12,8 +12,8 @@ tsnm="test_1"
 batch='16k'
 io_t='readwrite'
 io_s='10G'
-echo "================== ${tsnm} begins, i/o type ${batch}; batchsize ${batch}; i/o size ${io_s};  ===================" | tee ~/results.txt
-fio -direct=1 -iodepth 1 -thread  -ioengine=psync -rw=${io_t} -bs=${batch} -io_size=${io_s} -size=512M -numjobs=10 -runtime=60 -group_reporting  -fallocate=none -name=${tsnm} -directoryname=/mnt/orangefs | tee ~/results.txt
+echo "================== ${tsnm} begins, i/o type ${batch}; batchsize ${batch}; i/o size ${io_s};  ===================" | tee -a ~/results.txt
+fio -direct=1 -iodepth 1 -thread  -ioengine=psync -rw=${io_t} -bs=${batch} -io_size=${io_s} -size=512M -numjobs=10 -runtime=60 -group_reporting  -fallocate=none -name=${tsnm} -directoryname=/mnt/orangefs | tee -a ~/results.txt
 empty_env
 #
 # unalias
